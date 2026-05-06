a5fd8f126fbef76d6ab8fe65516f6f873af95e9a
Esteban <esgiraldop@unal.edu.co>
2026-05-06 08:39:41 -0500

    Se creó el output

--- ColabGit.cpp
+++ ColabGit.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
-#include <fstream>
-#include <>
-
+#include<fstream>
+#include<sstream>
 using namespace std;
 
 int main(){
-	ifstream input("./input/input.txt")
-	
-	if(input.is_open()){
+	ifstream input("./input/input.txt");
+	ofstream output("./output/output.txt");
+	if(input.is_open()&&output.is_open()){
 		cout << "Archivo Abierto :)" << endl;
 		string linea;
 		
 		while(getline(input, linea)){
-			cout << linea;
+			output << linea;
 		}
+		cout << "archivo esctito correctamente" << endl;
 	}
-	
+	output.close();
 	input.close();
 	return 0;
 }
