639c05385c737acd5ebdff129af5b08e83c3c04c
Jesús Afonso <jafonso@unal.edu.co>
2026-05-06 08:20:34 -0500

    Se añadió funcionalidad para leer archivos

--- ColabGit.cpp
+++ ColabGit.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include <fstream>
+#include <>
 
 using namespace std;
 
 int main(){
+	ifstream input("./input/input.txt")
 	
+	if(input.is_open()){
+		cout << "Archivo Abierto :)" << endl;
+		string linea;
+		
+		while(getline(input, linea)){
+			cout << linea;
+		}
+	}
+	
+	input.close();
 	return 0;
 }
