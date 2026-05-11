6e9faf0e156ca4524b36424373348a11d2f8d636
Andrea <andcardenasro@unal.edu.co>
2026-05-10 19:15:07 -0500

    Se añadió la conversión de la frase a mayúsculas y se añadió un archivo extra donde se escribe la frase en mayúsculas y también se añadió mi nombre Andrea y correo al archivo de Integrantes

--- ColabGit.cpp
+++ ColabGit.cpp
@@ -6,16 +6,27 @@ using namespace std;
 int main(){
 	ifstream input("./input/input.txt");
 	ofstream output("./output/output.txt");
+	ofstream output_mayus("./output/mayusculas.txt");
 	if(input.is_open()&&output.is_open()){
 		cout << "Archivo Abierto :)" << endl;
 		string linea;
-		
+		string mayus;
 		while(getline(input, linea)){
 			output << linea;
+				
+		for(int i = 0; i<linea.length()+1;i++){
+			mayus=toupper(linea[i]);
+			linea[i]=tolower(linea[i]);
+			output_mayus<<mayus;
+}
 		}
+		
+		
+
 		cout << "archivo esctito correctamente" << endl;
 	}
 	output.close();
 	input.close();
+	output_mayus.close();
 	return 0;
 }
